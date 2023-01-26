eb9394d63d322e9f25917303e41abf727c4913b8
Peter Schär <peter.schaer@be.ch>
2023-01-26 13:18:20 +0100

    #69906: Bugfix Umgebungsvariablen & Ablage Dump-Files

--- 50_import_data.sh
+++ 50_import_data.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-wget $OEREB_SERVER_DB_TRANSFERSTRUKTUR_URL -O transferstruktur.dump
-pg_restore -v -U "$POSTGRES_USER" -d $POSTGRES_DATABASE_APP transferstruktur.dump
+wget $POSTGRES_TRANSFERSTRUKTUR_URL -O /var/lib/postgresql/transferstruktur.dump
+pg_restore -v -U "$POSTGRES_USER" -d $POSTGRES_DATABASE_APP /var/lib/postgresql/transferstruktur.dump
 
-wget $OEREB_SERVER_DB_APPSCHEMA_URL -O appschema.dump
-pg_restore -v -U "$POSTGRES_USER" -d $POSTGRES_DATABASE_APP appschema.dump
+wget $POSTGRES_APPSCHEMA_URL -O /var/lib/postgresql/appschema.dump
+pg_restore -v -U "$POSTGRES_USER" -d $POSTGRES_DATABASE_APP /var/lib/postgresql/appschema.dump
 
-wget $OEREB_SERVER_DB_LOGSCHEMA_URL -O logschema.dump
-pg_restore -v -U "$POSTGRES_USER" -d $POSTGRES_DATABASE_LOG logschema.dump
+wget $POSTGRES_LOGSCHEMA_URL -O /var/lib/postgresql/logschema.dump
+pg_restore -v -U "$POSTGRES_USER" -d $POSTGRES_DATABASE_LOG /var/lib/postgresql/logschema.dump
